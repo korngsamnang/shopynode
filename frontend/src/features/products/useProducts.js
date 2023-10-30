37903c3ed9fda4a63e5771969b887baab3f1d96d
Korng Samnang <korngsamnang168@gmail.com>
2023-10-30 22:50:41 +0700

    Product pagination prefetching

--- frontend/src/features/products/useProducts.js
+++ frontend/src/features/products/useProducts.js
@@ -1,16 +1,31 @@
 import { getAllProducts } from "../../services/apiProduct.js";
 
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { useSearchParams } from "react-router-dom";
 
 export const useProducts = () => {
+    const queryClient = useQueryClient();
     const [searchParams] = useSearchParams();
     const keyword = searchParams.get("search") || "";
-    const page = searchParams.get("page") || 1;
+    const page = Number(searchParams.get("page")) || 1;
     const { data: products, isLoading } = useQuery({
         queryKey: ["products", keyword, page],
         queryFn: () => getAllProducts(keyword, page, 8),
     });
+    const totalPages = products?.totalPages;
+
+    if (page < totalPages) {
+        queryClient.prefetchQuery({
+            queryKey: ["products", keyword, page + 1],
+            queryFn: () => getAllProducts(keyword, page + 1, 8),
+        });
+    }
+    if (page > 1) {
+        queryClient.prefetchQuery({
+            queryKey: ["products", keyword, page - 1],
+            queryFn: () => getAllProducts(keyword, page - 1, 8),
+        });
+    }
 
     return {
         products,
